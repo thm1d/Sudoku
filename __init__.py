2dd4263dffc8fe8b4778f47433982c164d361a89
thm1d <tahmid11q@gmail.com>
2021-06-07 09:42:33 +0600

    Random Sudoku Maker File Added

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  7 09:39:11 2021
+
+@author: tahmi
+"""
+
+
---
+++ sudoku_maker.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  7 09:29:11 2021
+
+@author: tahmi
+"""
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+base  = 3
+side  = base*base
+
+# pattern for a baseline valid solution
+def pattern(r,c): return (base*(r%base)+r//base+c)%side
+
+# randomize rows, columns and numbers (of valid base pattern)
+from random import sample
+def shuffle(s): return sample(s,len(s)) 
+rBase = range(base) 
+rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ] 
+cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ]
+nums  = shuffle(range(1,base*base+1))
+
+# produce board using randomized baseline pattern
+board = [ [nums[pattern(r,c)] for c in cols] for r in rows ]
+
+for line in board: print(line)
+
+squares = side*side
+empties = squares * 3//4
+for p in sample(range(squares),empties):
+    board[p//side][p%side] = 0
+
+#print_board(board)
+#print(board)
